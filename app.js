4740da16e00bfa22ce081805a9e61a001bf0e402
Junichi TANAKA <junsan45@gmail.com>
2021-03-24 19:27:39 +0900

    Add button action

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.message("hello", async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
 app.event("app_mention", async ({ event, say }) => {
   const reversedText = [...event.text].reverse().join("");
   await say(reversedText);
